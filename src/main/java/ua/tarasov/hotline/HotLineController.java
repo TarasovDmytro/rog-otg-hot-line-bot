8a4b965715688ff00775fa2f585cefe386ece74b
TarasovDmytro <tarasov.personal@gmail.com>
2021-12-11 15:03:13 +0200

    refactoring

--- src/main/java/ua/tarasov/hotline/HotLineController.java
+++ src/main/java/ua/tarasov/hotline/HotLineController.java
@@ -1,101 +1,22 @@
 package ua.tarasov.hotline;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.methods.AnswerCallbackQuery;
-import org.telegram.telegrambots.meta.api.methods.send.SendLocation;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageReplyMarkup;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import ua.tarasov.hotline.handlers.CallBackQueryHandler;
-import ua.tarasov.hotline.handlers.MessageHandler;
-import ua.tarasov.hotline.models.model.ResponseContext;
 
-import java.util.List;
+@RestController
+public class HotLineController {
+    private final HotLineFasade fasade;
 
-@Component
-public class HotLineController extends TelegramLongPollingBot {
-    private final MessageHandler messageHandler;
-    private final CallBackQueryHandler callBackQueryHandler;
-
-    @Value("${telegram.bot.username}")
-    private String botUsername;
-
-    @Value("${telegram.bot.token}")
-    private String botToken;
-
-    public HotLineController(MessageHandler messageHandler, CallBackQueryHandler callBackQueryHandler) {
-        this.messageHandler = messageHandler;
-        this.callBackQueryHandler = callBackQueryHandler;
+    public HotLineController(HotLineFasade fasade) {
+        this.fasade = fasade;
     }
 
-    @Override
-    public String getBotUsername() {
-        return botUsername;
-    }
-
-    @Override
-    public String getBotToken() {
-        return botToken;
-    }
-
-    @Override
-    public void onUpdateReceived(Update update) {
-        if (update.hasCallbackQuery()) {
-            sendAnswerMessages(callBackQueryHandler.getResponseContext(update));
-        }
-
-        if (update.hasMessage()) {
-            sendAnswerMessages(messageHandler.getResponseContext(update));
-        }
-    }
-
-    public void sendAnswerMessages(ResponseContext response) {
-        List<SendMessage> answerMessages = response.getAnswerMessages();
-        List<AnswerCallbackQuery> answerCallbackQueries = response.getAnswerCallbackQueries();
-        List<EditMessageReplyMarkup> editMessageReplyMarkups = response.getEditMessageReplyMarkups();
-        List<SendLocation> sendLocations = response.getSendLocations();
-        if (answerMessages != null) {
-            answerMessages.forEach(message -> {
-                try {
-                    execute(message);
-                    Thread.sleep(35);
-                } catch (TelegramApiException | InterruptedException e) {
-                    e.printStackTrace();
-                }
-            });
-        }
-        if (answerCallbackQueries != null) {
-            answerCallbackQueries.forEach(message -> {
-                try {
-                    execute(message);
-                    Thread.sleep(35);
-                } catch (TelegramApiException | InterruptedException e) {
-                    e.printStackTrace();
-                }
-            });
-        }
-        if (editMessageReplyMarkups != null) {
-            editMessageReplyMarkups.forEach(message -> {
-                try {
-                    execute(message);
-                    Thread.sleep(35);
-                } catch (TelegramApiException | InterruptedException e) {
-                    e.printStackTrace();
-                }
-            });
-        }
-        if (sendLocations != null) {
-            sendLocations.forEach(message -> {
-                try {
-                    execute(message);
-                    Thread.sleep(35);
-                } catch (TelegramApiException | InterruptedException e) {
-                    e.printStackTrace();
-                }
-            });
-        }
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public BotApiMethod<?> onUpdateReceived(@RequestBody Update update) {
+        return fasade.onWebhookUpdateReceived(update);
     }
 }
---
+++ src/main/java/ua/tarasov/hotline/HotLineFasade.java
@@ -0,0 +1,130 @@
+package ua.tarasov.hotline;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramWebhookBot;
+import org.telegram.telegrambots.meta.api.methods.AnswerCallbackQuery;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.send.SendLocation;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageReplyMarkup;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import ua.tarasov.hotline.handlers.CallBackQueryHandler;
+import ua.tarasov.hotline.handlers.MessageHandler;
+import ua.tarasov.hotline.models.model.ResponseContext;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+
+@Component
+public class HotLineFasade extends TelegramWebhookBot {
+    private final MessageHandler messageHandler;
+    private final CallBackQueryHandler callBackQueryHandler;
+
+    @Value("${telegram.bot.username}")
+    private String botUsername;
+
+    @Value("${telegram.bot.token}")
+    private String botToken;
+
+    @Value("telegram.bot.path")
+    private String botPath;
+
+    public HotLineFasade(MessageHandler messageHandler, CallBackQueryHandler callBackQueryHandler) {
+        this.messageHandler = messageHandler;
+        this.callBackQueryHandler = callBackQueryHandler;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return botUsername;
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+
+    @Override
+    public String getBotPath() {
+        return botPath;
+    }
+
+//    @Override
+//    public void onUpdateReceived(Update update) {
+//        if (update.hasCallbackQuery()) {
+//            sendAnswerMessages(callBackQueryHandler.getResponseContext(update));
+//        }
+//
+//        if (update.hasMessage()) {
+//            sendAnswerMessages(messageHandler.getResponseContext(update));
+//        }
+//    }
+
+    @Override
+    public BotApiMethod<?> onWebhookUpdateReceived(Update update) {
+        BotApiMethod<?> botApiMethod = null;
+        if (update.hasCallbackQuery()) {
+            botApiMethod = sendAnswerMessages(callBackQueryHandler.getResponseContext(update));
+        }
+
+        if (update.hasMessage()) {
+            botApiMethod = sendAnswerMessages(messageHandler.getResponseContext(update));
+        }
+        return botApiMethod;
+    }
+
+    public BotApiMethod<?> sendAnswerMessages(ResponseContext response) {
+        AtomicReference<BotApiMethod<?>> botApiMethod = new AtomicReference<>(null);
+        List<List<? extends BotApiMethod<?>>> answerMessagesList = List.of(response.getAnswerMessages(), response.getAnswerCallbackQueries(),
+                response.getSendLocations(), response.getEditMessageReplyMarkups());
+//        List<SendMessage> answerMessages = response.getAnswerMessages();
+//        List<AnswerCallbackQuery> answerCallbackQueries = response.getAnswerCallbackQueries();
+//        List<EditMessageReplyMarkup> editMessageReplyMarkups = response.getEditMessageReplyMarkups();
+//        List<SendLocation> sendLocations = response.getSendLocations();
+//        if (answerMessages != null) {
+        answerMessagesList.forEach(answerMessages ->
+            answerMessages.forEach(message -> {
+                botApiMethod.set(message);
+//                try {
+//                    execute(message);
+//                    Thread.sleep(35);
+//                } catch (TelegramApiException | InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+            }));
+        return botApiMethod.get();
+        }
+//        if (answerCallbackQueries != null) {
+//            answerCallbackQueries.forEach(message -> {
+//                try {
+//                    execute(message);
+//                    Thread.sleep(35);
+//                } catch (TelegramApiException | InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            });
+//        }
+//        if (editMessageReplyMarkups != null) {
+//            editMessageReplyMarkups.forEach(message -> {
+//                try {
+//                    execute(message);
+//                    Thread.sleep(35);
+//                } catch (TelegramApiException | InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            });
+//        }
+//        if (sendLocations != null) {
+//            sendLocations.forEach(message -> {
+//                try {
+//                    execute(message);
+//                    Thread.sleep(35);
+//                } catch (TelegramApiException | InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            });
+//        }
+    }
+
