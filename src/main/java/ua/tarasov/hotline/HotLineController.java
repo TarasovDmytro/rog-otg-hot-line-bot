40990cd8f9a8483a7dd3df850d92c95e9d6c03e7
TarasovDmytro <tarasov.personal@gmail.com>
2021-12-11 21:41:21 +0200

    refactoring

--- src/main/java/ua/tarasov/hotline/HotLineController.java
+++ src/main/java/ua/tarasov/hotline/HotLineController.java
@@ -4,18 +4,20 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import ua.tarasov.hotline.models.model.RogOTGHotLineBot;
 
 @RestController
 @Slf4j
 public class HotLineController {
-private  final RogOTGHotLineBot bot;
+//private  final RogOTGHotLineBot bot;
 private final HotLineFacade facade;
 
-    public HotLineController(RogOTGHotLineBot bot, HotLineFacade facade) {
-        this.bot = bot;
+    public HotLineController(HotLineFacade facade) {
         this.facade = facade;
     }
+
+//    public HotLineController(RogOTGHotLineBot bot) {
+//        this.bot = bot;
+//    }
 
     @PostMapping("/")
     public BotApiMethod<?> onUpdateReceived(@RequestBody Update update) {
