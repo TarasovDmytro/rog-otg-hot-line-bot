046e8935cdea2851ade1e8a1e0539b6584f723e2
TarasovDmytro <tarasov.personal@gmail.com>
2022-01-14 18:01:07 +0200

    refactoring

--- src/main/java/ua/tarasov/hotline/models/models/RogOTGHotLineBot.java
+++ src/main/java/ua/tarasov/hotline/models/models/RogOTGHotLineBot.java
@@ -6,7 +6,6 @@ import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
