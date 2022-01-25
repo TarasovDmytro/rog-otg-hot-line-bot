a55ad3a898ccdcc43e888d9bb4fc57aba6c63ce0
TarasovDmytro <tarasov.personal@gmail.com>
2022-01-26 01:28:48 +0200

    refactoring

--- src/main/java/ua/tarasov/hotline/service/impl/ChatPropertyModeServiceImpl.java
+++ src/main/java/ua/tarasov/hotline/service/impl/ChatPropertyModeServiceImpl.java
@@ -10,9 +10,8 @@ import ua.tarasov.hotline.models.Department;
 import ua.tarasov.hotline.service.ChatPropertyModeService;
 
 @Service
-@FieldDefaults(level = AccessLevel.PRIVATE)
 public class ChatPropertyModeServiceImpl implements ChatPropertyModeService {
-    static ChatPropertyModeServiceImpl chatProperties;
+    private static ChatPropertyModeServiceImpl chatProperties;
 
     private ChatPropertyModeServiceImpl() {
     }
@@ -25,42 +24,52 @@ public class ChatPropertyModeServiceImpl implements ChatPropertyModeService {
         return chatProperties;
     }
 
+    @Override
     public Department getCurrentDepartment(long chatId) {
         return currentDepartment.getOrDefault(chatId, Department.JEU_DOKUCHAEVSKE);
     }
 
+    @Override
     public void setCurrentDepartment(long chatId, Department department) {
         currentDepartment.put(chatId, department);
     }
 
+    @Override
     public Boolean getCurrentAdminKeyboardState(Long chatId) {
         return currentAdminKeyboardState.getOrDefault(chatId, true);
     }
 
+    @Override
     public void setCurrentAdminKeyboardState(long chatId, boolean adminKeyboardState) {
         currentAdminKeyboardState.put(chatId, adminKeyboardState);
     }
 
+    @Override
     public void setBotState(long chatId, BotState botState) {
         currentBotState.put(chatId, botState);
     }
 
+    @Override
     public BotState getCurrentBotState(long chatId) {
         return currentBotState.getOrDefault(chatId, BotState.WAIT_BUTTON);
     }
 
+    @Override
     public void setCurrentRequestAddress(long chatId, String address) {
         currentRequestAddress.put(chatId, address);
     }
 
+    @Override
     public String getCurrentRequestAddress(long chatId) {
         return currentRequestAddress.getOrDefault(chatId, "Адресу не встановлено");
     }
 
+    @Override
     public void setCurrentLocation(long chatId, Location location) {
         currentLocation.put(chatId, location);
     }
 
+    @Override
     public Location getCurrentLocation(long chatId) {
         return currentLocation.get(chatId);
     }
