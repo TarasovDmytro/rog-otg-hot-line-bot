c041ce0999729a0c8e9d7ff75b88c73a13d9c5bd
TarasovDmytro <tarasov.personal@gmail.com>
2022-01-30 21:57:57 +0200

    refactoring

--- src/main/java/ua/tarasov/hotline/service/impl/NotificationParseServiceImpl.java
+++ src/main/java/ua/tarasov/hotline/service/impl/NotificationParseServiceImpl.java
@@ -56,19 +56,19 @@ public class NotificationParseServiceImpl implements NotificationParseService {
                     notificationService.saveUpdateNotification(notification);
                     updateNotifications.add(notification);
                 }
-                else {
-                    if (notificationService.findByDate(date).isPresent()) {
-                        Notification updateNotification = notificationService.findByDate(date).get();
-                        log.info(String.valueOf(updateNotification));
-                        if (!updateNotification.getLink().equals(link)) {
-                            updateNotification.setLink(link);
-                            log.info(String.valueOf(updateNotification));
-                            notificationService.saveUpdateNotification(updateNotification);
-                            log.info(String.valueOf(updateNotification));
-                            updateNotifications.add(updateNotification);
-                        }
-                    }
-                }
+//                else {
+//                    if (notificationService.findByDate(date).isPresent()) {
+//                        Notification updateNotification = notificationService.findByDate(date).get();
+//                        log.info(String.valueOf(updateNotification));
+//                        if (!updateNotification.getLink().equals(link)) {
+//                            updateNotification.setLink(link);
+//                            log.info(String.valueOf(updateNotification));
+//                            notificationService.saveUpdateNotification(updateNotification);
+//                            log.info(String.valueOf(updateNotification));
+//                            updateNotifications.add(updateNotification);
+//                        }
+//                    }
+//                }
             });
         } catch (IOException e) {
             e.printStackTrace();
