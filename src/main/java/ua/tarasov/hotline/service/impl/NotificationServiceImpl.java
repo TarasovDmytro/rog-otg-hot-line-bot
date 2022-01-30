339e46c08f443c4b0170d49e137700af85c8b0d0
TarasovDmytro <tarasov.personal@gmail.com>
2022-01-30 22:39:23 +0200

    refactoring

--- src/main/java/ua/tarasov/hotline/service/impl/NotificationServiceImpl.java
+++ src/main/java/ua/tarasov/hotline/service/impl/NotificationServiceImpl.java
@@ -39,7 +39,7 @@ public class NotificationServiceImpl implements NotificationService {
 
     @Override
     public List<Notification> findAll() {
-        return repository.findAll();
+        return repository.findAll(Sort.by("date"));
     }
 
     @Override
