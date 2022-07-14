70de9fc953c2fb2c01f2514cd8a40ab6fc1d91d0
Dmytro Tarasov <tarasov.personal@gmail.com>
2022-07-14 17:54:54 +0300

    some change

--- src/main/java/ua/tarasov/hotline/controller/impl/MessageController.java
+++ src/main/java/ua/tarasov/hotline/controller/impl/MessageController.java
@@ -53,7 +53,7 @@ public class MessageController implements Controller {
             List<BotUser> botUsers = botUserService.findAll();
             botUsers.forEach(botUser -> answerMessages.add(SendMessage.builder()
                     .chatId(String.valueOf(botUser.getId()))
-                    .text(message.getText() + message.getVideo())
+                    .text(message.getText())
                     .parseMode("HTML")
                     .build()));
             chatPropertyModeService.setCurrentBotState(message.getChatId(), BotState.WAIT_BUTTON);
