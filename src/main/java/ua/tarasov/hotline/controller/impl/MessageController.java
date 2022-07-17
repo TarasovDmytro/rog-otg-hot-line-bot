7eb18ca4495bf4aa753f3b24f67e2138a4534b25
Dmytro Tarasov <tarasov.personal@gmail.com>
2022-07-18 01:45:34 +0300

    some change

--- src/main/java/ua/tarasov/hotline/controller/impl/MessageController.java
+++ src/main/java/ua/tarasov/hotline/controller/impl/MessageController.java
@@ -56,20 +56,20 @@ public class MessageController implements Controller {
         if (checkRoleService.checkIsAdmin(message.getChatId())) {
             List<BotApiMethod<?>> answerMessages = new ArrayList<>();
             List<BotUser> botUsers = botUserService.findAll();
-            if (message.hasText()) {
-                botUsers.forEach(botUser ->
-                        answerMessages.add(SendMessage.builder()
-                                .chatId(String.valueOf(botUser.getId()))
-                                .text(message.getText())
-                                .parseMode("HTML")
-                                .build()));
-            }
-            if (message.hasVideo()) {
+//            if (message.hasText()) {
+//                botUsers.forEach(botUser ->
+//                        answerMessages.add(SendMessage.builder()
+//                                .chatId(String.valueOf(botUser.getId()))
+//                                .text(message.getText())
+//                                .parseMode("HTML")
+//                                .build()));
+//            }
+//            if (message.hasVideo()) {
                 botUsers.forEach(botUser ->
                         answerMessages.add(CopyMessage.builder()
                         .chatId(String.valueOf(botUser.getId()))
                         .build()));
-            }
+//            }
             chatPropertyModeService.setCurrentBotState(message.getChatId(), BotState.WAIT_BUTTON);
             return answerMessages;
         }
